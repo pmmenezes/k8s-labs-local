44d7aa04e3aa952833f29d76b5c041bc9fdd438d
Pablo M Menezes <pmenezes.aju@gmail.com>
2022-11-10 22:28:08 -0300

    Script containerd

---
+++ vagrant/provision.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+modprobe overlay
+modprobe br_netfilter
+cat <<EOF > /etc/modules-load.d/k8s.conf
+        overlay
+        br_netfilter
+EOF
+
+# Disabilita memoria swap  
+sudo swapoff -a
+(crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
+
+# sysctl params required by setup, params persist across reboots
+
+cat <<EOF > /etc/sysctl.d/k8s.conf
+        net.bridge.bridge-nf-call-iptables  = 1
+        net.bridge.bridge-nf-call-ip6tables = 1
+        net.ipv4.ip_forward = 1
+EOF
+
+## Apply sysctl params without reboot
+sysctl --system
+
+# Pacotes Necessarios
+apt-get install -y apt-transport-https ca-certificates curl gnupg2 lsb-release
+
+# Instalação containerd 
+mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update && apt install -y  containerd.io 
+mkdir -p /etc/containerd
+containerd config default > /etc/containerd/config.toml
+
+# Instalação kubeadm Kubelete kubectl 
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt update && apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin  curl apt-transport-https -y
+
+apt install -y  kubelet kubectl kubeadm
+apt-mark hold kubelet kubeadm kubectl
+systemctl enable kubelet
+
+# Habilitando Cgroup Systemd no containerd 
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+
+systemctl restart containerd
+
